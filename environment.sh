d04730ee36e7e2fa05168e020357ff99bf3d6654
Priidu Kull <priidu.kull@gmail.com>
2014-12-25 22:50:27 +0200

    A different setup was used.

--- environment.sh
+++ environment.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-vagrant init puphpet/debian75-x64
-vagrant up
-vagrant ssh
-wget https://www.python.org/ftp/python/3.4.2/Python-3.4.2.tar.xz
-tar xf Python-3.4.2.tar.xz
-cd Python-3.4.2
-sudo aptitude install gcc
-./configure --prefix /opt/python/3.4.2
-sudo make
-sudo make install
-export PATH=/opt/python/3.4.2/bin:$PATH
-cd ..
-pyvenv fb1
-source fb1/bin/activate
+sudo apt-get install postgresql postgresql-contrib
+psql facebook < facebook.sql
+sudo apt-get install python3-pip
+sudo apt-get install libpq-dev
+sudo pip3 install psycopg2
+sudo pip3 install -r requirements.txt
