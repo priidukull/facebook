1c43019f0e175bf45f5fa0325f5566fb78a90382
Priidu Kull <priidu.kull@gmail.com>
2014-12-25 22:40:34 +0200

    Now the back office has two pages: to display the contents of the news table and to display all the posts.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from flask import render_template
+
 from app import app
+from models import News, Post
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    news = News().all()
+    return render_template('index.html',
+                           news=news)
+
+@app.route('/post')
+def post():
+    news = News().all()
+    posts = [Post(a_news) for a_news in news]
+    return render_template('post.html',
+                           posts=posts)
