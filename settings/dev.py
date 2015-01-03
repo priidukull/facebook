fa17a9a0d7fbf13b0fdb8a01dfec482a2f57aa69
Priidu Kull <priidu.kull@gmail.com>
2015-01-03 21:52:32 +0200

    To make the web app accessible through the world wide web.

--- settings/dev.py
+++ settings/dev.py
@@ -2,5 +2,6 @@ from settings.default import USER_ID
 
 APP_ID = '1522386624678645'
 APP_SECRET = '721b9fef325ca05605b102bec5e2f7dc'
+HOST = '127.0.0.1'
 PAGE_ACCESS_TOKEN = '1522386624678645|IaRAAKIm1XGjsZhOn4Df7uN4GXk'
 PAGE_ID = USER_ID  # page is user's personal page
