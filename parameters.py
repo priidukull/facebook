22e9cdc15e5166b2fcb6af33a8bc0ebcd4a16b11
Priidu Kull <priidu.kull@gmail.com>
2015-01-21 21:06:32 +0200

    May be a different way of presenting code will help us.

--- parameters.py
+++ parameters.py
@@ -5,11 +5,14 @@ import re
 
 action_var = os.getenv('ACTION')
 
-def _load_settings(mode):
-    module = importlib.import_module('settings.' + mode)
+def _load_settings():
+    module = importlib.import_module('settings.default')
+    settings = {k: v for k, v in module.__dict__.items()
+                if not re.match('^(_|@)', k)}
+    globals().update(settings)
+    module = importlib.import_module('settings.' + os.environ.get('MODE'))
     settings = {k: v for k, v in module.__dict__.items()
                 if not re.match('^(_|@)', k)}
     globals().update(settings)
 
-for e in ['default', os.environ.get('MODE')]:
-    _load_settings(e)
+_load_settings()
